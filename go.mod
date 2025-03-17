897902c173c7cc1bb993f4b0f50375c5b64d5422
JoeyShapiro <joeyashapiro@gmail.com>
2025-03-16 21:12:35 -0500

    it works

---
+++ go.mod
@@ -0,0 +1,3 @@
+module malaina
+
+go 1.22.5
---
+++ main.go
@@ -0,0 +1,147 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+)
+
+func main() {
+	queue := []int{4081}
+	seen := []int{}
+	watchTime := 0
+	episodes := 0
+
+loop_queue:
+	for len(queue) > 0 {
+		id := queue[0]
+		queue = queue[1:]
+
+		for _, idSeen := range seen {
+			if id == idSeen {
+				continue loop_queue
+			}
+		}
+		seen = append(seen, id)
+
+		media, err := queryAnime(id)
+		if err != nil {
+			fmt.Println(id, ":", err)
+			if err.Error() == "Too Many Requests." {
+				queue = append(queue, id)
+				time.Sleep(60 * time.Second)
+			}
+			continue
+		}
+
+		watchTime += media.Duration * media.Episodes
+		episodes += media.Episodes
+
+		for _, related := range media.Relations.Edges {
+			queue = append(queue, related.Node.Id)
+		}
+
+		fmt.Printf("%+v\n", media)
+	}
+
+	fmt.Println("Total watch time:", watchTime/60, "hours")
+}
+
+type Response struct {
+	Data struct {
+		Media Media `json:"Media"`
+	} `json:"data"`
+	Errors []struct {
+		Message string `json:"message"`
+	} `json:"errors"`
+}
+
+type Media struct {
+	Id       int    `json:"idMal"`
+	Episodes int    `json:"episodes"`
+	Duration int    `json:"duration"`
+	Format   string `json:"format"`
+	Title    struct {
+		English string `json:"english"`
+		Romaji  string `json:"romaji"`
+	} `json:"title"`
+	Relations struct {
+		Edges []struct {
+			RelationType string `json:"relationType"`
+			Node         struct {
+				Id int `json:"id"`
+			} `json:"node"`
+		} `json:"edges"`
+	} `json:"relations"`
+}
+
+func queryAnime(id int) (media Media, err error) {
+	// Define the GraphQL query
+	query := map[string]string{
+		"query": fmt.Sprintf(`
+			{
+				Media(idMal: %d) {
+					idMal,
+					episodes,
+					duration,
+					format,
+					title {
+						english,
+						romaji
+					},
+					relations {
+						edges {
+							relationType,
+							node {
+								id
+							}
+						}
+					}
+				}
+			}
+		`, id),
+	}
+	jsonValue, _ := json.Marshal(query)
+
+	// Send the HTTP request
+	request, _ := http.NewRequest("POST", "https://graphql.anilist.co", bytes.NewBuffer(jsonValue))
+	request.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+		return
+	}
+	defer response.Body.Close()
+
+	// Parse the response
+	data, _ := io.ReadAll(response.Body)
+	var res Response
+	if err := json.Unmarshal(data, &res); err != nil {
+		return media, err
+	}
+
+	if len(res.Errors) > 0 {
+		return media, fmt.Errorf(res.Errors[0].Message)
+	}
+
+	return res.Data.Media, nil
+}
+
+/*
+{
+  show(id: "1") {
+    ...ShowWithSequels
+  }
+}
+
+fragment ShowWithSequels on Show {
+  title
+  sequel {
+    ...ShowWithSequels
+  }
+}
+*/
