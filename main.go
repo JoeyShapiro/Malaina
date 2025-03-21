4e9e62d034cb8f10f612f2110e6ae84d10477dc7
JoeyShapiro <joeyashapiro@gmail.com>
2025-03-20 21:14:41 -0500

    create graph

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
+	"html/template"
 	"io"
 	"net/http"
 	"os"
@@ -13,16 +14,87 @@ import (
 
 func main() {
 	fid := flag.Int("id", 0, "ID of the anime to start from")
+	fimport := flag.String("import", "", "Import a json file")
 
 	flag.Parse()
 
-	if *fid == 0 {
-		fmt.Println("Please provide an ID")
+	if *fid == 0 && *fimport == "" {
+		fmt.Println("Please provide an ID or a json file")
 		os.Exit(1)
 	}
 
 	var medias []Media
-	queue := []int{*fid}
+	var err error
+	if *fid != 0 {
+		medias, err = queryAnimes(*fid)
+		if err != nil {
+			fmt.Println("Error querying anime:", err)
+			os.Exit(1)
+		}
+	} else {
+		content, err := os.ReadFile(*fimport)
+		if err != nil {
+			fmt.Println("Error reading file:", err)
+			os.Exit(1)
+		}
+		if err := json.Unmarshal(content, &medias); err != nil {
+			fmt.Println("Error parsing json:", err)
+			os.Exit(1)
+		}
+	}
+
+	// // write medias to json file
+	// data, _ := json.Marshal(medias)
+	// os.WriteFile("medias.json", data, 0644)
+
+	watchTime := 0
+	episodes := 0
+	var links []Link
+
+	// do stuff with it separately
+	for _, media := range medias {
+		watchTime += media.Duration * media.Episodes
+		episodes += media.Episodes
+
+		for _, related := range media.Relations.Edges {
+			links = append(links, Link{
+				media.Id,
+				related.Node.Id,
+				related.RelationType})
+		}
+	}
+
+	// Create some data to pass to the template
+	page := PageData{
+		Title:     medias[0].Title.English,
+		Medias:    medias,
+		Links:     links,
+		Episodes:  episodes,
+		WatchTime: watchTime,
+	}
+
+	// Parse the template file
+	tmpl, err := template.ParseFiles("template.html")
+	if err != nil {
+		fmt.Println("Error parsing template:", err)
+		os.Exit(1)
+	}
+
+	// Execute the template with our data
+	f, err := os.Create("medias.html")
+	if err != nil {
+		fmt.Println("Error creating output file:", err)
+		os.Exit(1)
+	}
+	err = tmpl.Execute(f, page)
+	if err != nil {
+		fmt.Println("Error executing template:", err)
+		os.Exit(1)
+	}
+}
+
+func queryAnimes(id int) (medias []Media, err error) {
+	queue := []int{id}
 	seen := []int{}
 
 loop_queue:
@@ -54,29 +126,15 @@ loop_queue:
 		medias = append(medias, media)
 	}
 
-	// write medias to json file
-	data, _ := json.Marshal(medias)
-	os.WriteFile("medias.json", data, 0644)
+	return
+}
 
-	watchTime := 0
-	episodes := 0
-	var links []Link
-
-	// do stuff with it separately
-	for _, media := range medias {
-		watchTime += media.Duration * media.Episodes
-		episodes += media.Episodes
-
-		for _, related := range media.Relations.Edges {
-			links = append(links, Link{
-				media.Id,
-				related.Node.Id,
-				related.RelationType})
-		}
-	}
-
-	fmt.Println("Total watch time:", watchTime/60, "hours")
-	fmt.Println("Total episodes:", episodes)
+type PageData struct {
+	Title     string
+	Medias    []Media
+	Links     []Link
+	Episodes  int
+	WatchTime int
 }
 
 type Response struct {
